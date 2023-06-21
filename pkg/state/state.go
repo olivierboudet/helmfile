b6ba1a23b342a3b0a378965ad4b8f3870ee76f22
Olivier Boudet <o.boudet@gmail.com>
2023-06-21 15:04:12 +0200

    feat(lock): allow to use lock file by environment with different chart versions #870
    Signed-off-by: Olivier Boudet <olivier.boudet@cooperl.com>

--- pkg/state/state.go
+++ pkg/state/state.go
@@ -1091,6 +1091,15 @@ type PrepareChartKey struct {
 func (st *HelmState) PrepareCharts(helm helmexec.Interface, dir string, concurrency int, helmfileCommand string, opts ChartPrepareOptions) (map[PrepareChartKey]string, []error) {
 	var selected []ReleaseSpec
 
+	// resolve deps before anything else to have correct version from eventual lock file
+	if !opts.SkipResolve {
+		updated, err := st.ResolveDeps()
+		if err != nil {
+			return nil, []error{err}
+		}
+		*st = *updated
+	}
+
 	if len(st.Selectors) > 0 {
 		var err error
 
@@ -1114,14 +1123,6 @@ func (st *HelmState) PrepareCharts(helm helmexec.Interface, dir string, concurre
 
 	jobQueue := make(chan *ReleaseSpec, len(releases))
 	results := make(chan *chartPrepareResult, len(releases))
-
-	if !opts.SkipResolve {
-		updated, err := st.ResolveDeps()
-		if err != nil {
-			return nil, []error{err}
-		}
-		*st = *updated
-	}
 
 	var builds []*chartPrepareResult
 
